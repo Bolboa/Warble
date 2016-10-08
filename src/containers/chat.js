ac595ecf0e69e25725b5aee0d6f11e214662a8d1
Ese <eseinyang2005@hotmail.com>
2016-10-07 23:55:43 -0400

    Hey

--- src/containers/chat.js
+++ src/containers/chat.js
@@ -17,8 +17,8 @@ class Chat extends Component {
 	        audio: false
     	};
     	if(navigator.getUserMedia) {
-	        navigator.getUserMedia(constraints, this.getUserMediaSuccess, this.getUserMediaError);
-	        
+	        navigator.getUserMedia(constraints, this.getUserMediaSuccess.bind(this), this.getUserMediaError.bind(this));
+
 	    } else {
 	        alert('Your browser does not support getUserMedia API');
 	    }
@@ -26,11 +26,13 @@ class Chat extends Component {
 
 
 	}
+
 	getUserMediaSuccess(stream) {
 
 		//var localStream = stream;
 		var vid = this.refs.localStream;
 		vid.src = window.URL.createObjectURL(stream);
+		vid.play();
 
 
 	}
@@ -38,16 +40,16 @@ class Chat extends Component {
 	getUserMediaError(err) {
 		console.log(err);
 	}
- 
+
 
 	render(){
     	return(
       <div>
  		<h1>Chat Page</h1>
  		<div>Welcome, {this.props.username}</div>
- 		<video ref="localStream" width="320" height="240" controls></video>
+ 		<video ref="localStream" width="320" height="240" ></video>
 
-      
+
       </div>
     )
   }
