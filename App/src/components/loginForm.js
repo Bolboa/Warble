572ec98b00d468ccd51a89062dbc8795bd46873e
Eric Bolboa <13eb10@queensu.ca>
2016-12-26 03:09:06 -0500

    restAPI documentation

--- App/src/components/loginForm.js
+++ App/src/components/loginForm.js
@@ -5,38 +5,28 @@ export default class Login extends Component{
 	render(){
         return(
         	<div className="bodyLogin">
-        		
         		<img className="bg_image" src={require("../../images/background.jpg")}/>
-            
             	<div className="loginForm">
-            	
             		<div className="loginWrap">
-            		
             			<img className="logo_image" src={require("../../images/logo.png")}/>
-            
             			<h1>Login</h1>
-			    
 			    		<div>
 			        		<label>USERNAME</label>
 			        		<input type="text" name="username"  onChange={this.props.username}/>
 			        		<div className="errUsername">{this.props.errUser}</div>
 			    		</div>
-
 			    		<div>
 			        		<label>PASSWORD</label>
 			        		<input type="password" name="password" onChange={this.props.password}/>
 			        		<div className="errPassword">{this.props.errPass}</div>
 			    		</div>
-			    
 			    		<div>
 			        		<button className="login_btn" onClick={this.props.submit}>Login</button>
 			    		</div>
-			    
 			    		<div className="registerLinkWrap">
 			    			<p className="register_sentence">Don't have an account?</p>
 			    			<a className="register_link" onClick={this.props.switch} href='#'>Register</a>
 			    		</div>
-			    	
 			    	</div>
 				</div>
 			</div>
--- restAPI/index.js
+++ restAPI/index.js
@@ -4,49 +4,49 @@ var port     = process.env.PORT || 8000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
-
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var jwt = require('jwt-simple');
 var session = require('express-session');
 
+//connect to MongoLab database
 mongoose.connect('mongodb://Savage Tekk:2savages@ds061506.mlab.com:61506/user');
-var db = mongoose.connection;
 
-
-// require('./config/passport')(passport); // pass passport for configuration
+//sets up routes for server requests
 var router = express.Router();
-// set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
 
-//app.use(bodyParser.json({ type: 'application/*+json' }))
+// log every request to the console
+app.use(morgan('dev'));
+
+//sets necessary headers for server requests
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+	res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
+//body parser to read json format
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// required for passport
 
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+//sets the session secret
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+//initialize Passport.js
 app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
-//app.use(cors());
-// routes ======================================================================
+//persistent login sessions
+app.use(passport.session()); 
+//use connect-flash for flash messages stored in session (throws error messages in console)
+app.use(flash()); 
 
+//load our routes and pass in our app and fully configured passport
+require('./config/app.js')(app, router, passport, jwt); 
 
-require('./config/app.js')(app, router, passport, jwt); // load our routes and pass in our app and fully configured passport
-
-
-
+//route to check if API is working
 app.use('/api', router);
-// launch ======================================================================
+
+//launch API
 app.listen(port);
 console.log('The magic happens on port ' + port);
